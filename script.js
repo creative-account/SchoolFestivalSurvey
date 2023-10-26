d0906f27c9f14993b36b1356974c1a518dee6ac1
Nakano Yuta <102407879+creative-account@users.noreply.github.com>
2023-10-27 08:27:28 +0900

    ページ遷移

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let submitted = false;
-
 function load_animation(){
     button = document.getElementById("submit");
     button.setAttribute("class", "button is-primary is-loading");
+    location.href = "thanks.html"
 }
