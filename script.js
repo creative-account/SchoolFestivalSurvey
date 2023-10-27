dec68b4b57b60df8438edf867ca265d3b7cb87d6
Nakano Yuta <102407879+creative-account@users.noreply.github.com>
2023-10-27 11:27:59 +0900

    フォームの送信処理

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let submitted = false;
+button = document.getElementById("submit");
 
-function load_animation(){
-    button = document.getElementById("submit");
+function submitting(){
     button.setAttribute("class", "button is-primary is-loading");
+    submitted = true;
 }
